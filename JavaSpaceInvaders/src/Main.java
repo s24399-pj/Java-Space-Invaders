089f9edd17cbfb4ad57d080025c5fa425ad2bab3
alopalka <alopalka@MacBook-Pro-alopalka.local>
2022-06-13 18:26:05 +0200

    Project Creation
    First commit implementing Simple game loop + jframe setup

---
+++ JavaSpaceInvaders/src/Main.java
@@ -0,0 +1,23 @@
+import javax.swing.JFrame;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Space Invaders");
+
+        Screen screen=new Screen();
+        window.add(screen);
+
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        screen.startGameThread();
+
+    }
+}
